bc77361309fae138b2140826f5123d5f0d3ddfe3
ivo2666 <ivailocvetkov365@gmail.com>
2022-10-31 18:52:35 +0200

    fix: getData

--- src/api/news.js
+++ src/api/news.js
@@ -7,8 +7,7 @@ const params = [
 
 export const getData = async () => {
   const res = await fetch(`/api/news?${params.join("&")}`);
-  const data = await res.json();
-  console.log(data)
+  const {data} = await res.json();
   return data || mockData;
 };
 
